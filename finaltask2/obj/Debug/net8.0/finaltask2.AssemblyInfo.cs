b8c5848011da4d0fb9e004478585892a3641cb66
Kyda4 <studiokyda@yandex.ru>
2025-01-14 02:24:57 +0500

    При выполнение задачи изменились файлы, также добавили эти изменения в гит

--- finaltask2/obj/Debug/net8.0/finaltask2.AssemblyInfo.cs
+++ finaltask2/obj/Debug/net8.0/finaltask2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finaltask2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fde399d7dc13b54366b5fb1dec4910d5bfdf90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7bab53b82c9d65eeec62e68ec6a1d805e56139")]
 [assembly: System.Reflection.AssemblyProductAttribute("finaltask2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finaltask2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
